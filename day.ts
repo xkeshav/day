6f3f2bb409f10c68a5906ca928cd67dae336e9bc
Keshav Mohta <xkeshav@gmail.com>
2024-02-29 18:31:37 +0530

    deno package basic boilerplate

---
+++ day.ts
@@ -0,0 +1,2 @@
+/* this property return number of milliseconds in a day */
+export const oneDay = 5 ** 5 * 4 ** 4 * 3 ** 3 * 2 ** 2 * 1 ** 1;
---
+++ index.ts
@@ -0,0 +1,16 @@
+// mod.ts
+/**
+ * A module return number of milliseconds in a day
+ *
+ * @example
+ * ```ts
+ * import { oneDay } from "@xkeshav/day";
+ *
+ * console.log({oneDay}); // return { oneDay : 86400000 }
+ * ```
+ *
+ * @module
+ */
+
+export * from "./day.ts";
+
